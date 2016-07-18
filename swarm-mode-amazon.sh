b227c7f79685c0ef68710fb3526e9c288f9a9dfa
Arun Gupta <arun.gupta@gmail.com>
2016-07-18 07:55:01 -0500

    added instructions to launch three instances, added comments

--- swarm-mode-amazon.sh
+++ swarm-mode-amazon.sh
@@ -1,7 +1,13 @@
 set -x
+
+# Start three Ubuntu instances
+#aws ec2 run-instances --image-id ami-06116566 --count 3 --instance-type t2.micro --key-name arun@couchbase --security-groups swarm-mode
+
+# Get all the public and private IP addresses
 publicIp=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq -r .Reservations[].Instances[].PublicDnsName`
 privateIp=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq -r .Reservations[].Instances[].PrivateDnsName`
 
+# Install experimental Docker
 for node in $publicIpList
 do
     ssh -o StrictHostKeyChecking=no -i ~/.ssh/aruncouchbase.pem ubuntu@$node 'curl -fsSL https://experimental.docker.com/ | sh'
@@ -10,6 +16,7 @@ do
 done
 set -x
 
+# Convert the IP addresses into an array
 publicIp=($publicIpList)
 privateIp=($privateIpList)
 
